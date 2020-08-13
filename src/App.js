a74bb640b896ff47c4489f00c9ac0af1eff9f651
muhammadusamabaig <muhammadusamabaig099@gmail.com>
2020-08-12 22:37:21 -0700

    form error resolve

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core' 
 import Login from './Component/screens/Login'
 import Singup from './Component/screens/Signup'
-
+import {Drawer,List,ListItem,ListItemText,ListItemIcon} from '@material-ui/core'
+import HomeIcon from '@material-ui/icons/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
-
+// import classes from '*.module.css';
+const usestyle= makeStyles((theme)=>({
+drawerPaper:{width:"inherit"}
+}))
 function App() {
+  const classes=usestyle()
   return (
     <div className="globalcolor" >
+<Router>
+  <Drawer style={{width:"240px"}} variant="persistent" anchor="left" open={true} classes={{paper:classes.drawerPaper}}>
+<ListItem>
+  <ListItemIcon>
+    <HomeIcon/>
+  </ListItemIcon>
+  <ListItemText primary="Home"/>
+</ListItem>
 
-<Singup/>
+
+
+
+  </Drawer>
+</Router>
+{/* <Singup/> */}
    </div>
   );
 }
--- src/Component/screens/Signup.js
+++ src/Component/screens/Signup.js
@@ -82,38 +82,38 @@ function SingUp() {
     //     }
     // }
     return (
-        <div style={{ height: "120vh", width: "100%", overflow: "hidden" }}>
+        <div style={{ height: "100vh", width: "100%", overflow: "hidden" }}>
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12 text-center" style={{color:"white"}}><h3>SingUp</h3></div>
-                    <form className="col-10 ">
-
-                        <div className="form-group">
+                    <div className="col-10 ">
+                        
+                    <div >
                             <label style={{color:"white"}}>First name</label>
                             <input type="text" onChange={(e) => { setname(e.target.value) }} className="form-control" placeholder="First name" />
                         </div>
 
-                        <div className="form-group">
+                        <div>
                             <label style={{color:"white"}}>Father name</label>
                             <input onChange={(e) => { setfname(e.target.value) }} type="text" className="form-control" placeholder="Last name" />
                         </div>
 
-                        <div className="form-group">
+                        <div>
                             <label style={{color:"white"}}>Email address</label>
                             <input onChange={(e) => { setgmail(e.target.value) }} type="email" className="form-control" placeholder="Enter email" />
                         </div>
 
-                        <div className="form-group">
+                        <div >
                             <label style={{color:"white"}}>Cnic no</label>
                             <input onChange={(e) => { setcnic(e.target.value) }} type="number"  className="form-control" placeholder="Enter Cnic" />
                         </div>
 
-                        <div className="form-group">                                                     
+                        <div >                                                     
                             <label style={{color:"white"}}>Address</label>
-                            <input onChange={(e) => { setaddress(e.target.value) }} type="text"  className="form-control" placeholder="Enter Address" />
+                            <input onChange={(e) => { setaddress(e.target.value) }} type="text" className="form-control"  placeholder="Enter Address" />
                         </div>
 
-                        <div className="form-group">
+                        <div >
                             {/* <label>Choose Cetagory</label> */}
                             <Form.Label style={{color:"white"}}>Disabled select menu</Form.Label>
                             <Form.Control onChange={(e) => { setselectpersone(e.target.value) }} as="select">
@@ -125,16 +125,17 @@ function SingUp() {
                         </div>
 
 
-                        <div className="form-group">
+                        <div >
                             <label style={{color:"white"}}>Password</label>
                             <input onChange={(e) => { setpassword(e.target.value) }} type="password"  className="form-control" placeholder="Enter password" />
                         </div>
 
-                        <button type="submit"className="btn btn-primary btn-block">Sign Up</button>
+                        <button type="submit"className="btn btn-primary btn-block" style={{marginTop:"2%"}}>Sign Up</button>
                         <p className="forgot-password text-right" style={{color:"white"}}>
                             Already registered <a href="#">sign in?</a>
                         </p>
-                    </form>
+
+                    </div>
                 </div>
             </div>
         </div>
